7f058de74c9166ab23bf0bb29e1241426a65aeae
Leon-kings <leonakingeneye2002@gmail.com>
2025-01-27 15:22:46 -0800

    changes on new postController

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { createPost, getPosts } from "../controller/postController.js";
 import upload from "../helpers/multer.js";
-import Authorization from "../middlewares/authorization.js";
+// import Authorization from "../middlewares/authorization.js";
 
 const router = express.Router();
 
 // Define routes
-router.post("/", upload.single("image"), Authorization, createPost);
+router.post("/", upload.single("image"), createPost);
 router.get("/", getPosts);
 
 export default router;
